97534477b24f09cda11c30ac04b6d94987173405
sakiko-otaka <teri_2012@icloud.com>
2023-11-13 22:25:17 +0900

    コミット

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,8 +5,8 @@
     <th>Name</th>
     <th>Description</th>
     <th>Price</th>
-    <th>Category</th>
-    <th>Action</th>
+    <th>Category ID</th>
+    <th >Action</th>
   </tr>
   @foreach ($products as $product)
   <tr>
@@ -15,7 +15,7 @@
     <td>{{ $product->price }}</td>
     <td>{{ $product->category_id }}</td>
     <td>
-      <form action="{{ rooute('products.destroy', $product->id) }}" method="POST">
+      <form action="{{ route('products.destroy', $product->id) }}" method="POST">
       <a href="{{ route('products.show', $product->id) }}">Show</a>
       <a href="{{ route('products.edit', $product->id) }}">Edit</a>
       @csrf
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('product', ProductController::class);
+Route::resource('products', ProductController::class)->middleware(['auth', 'verified']);
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
